f1e2dc0e1edcbd21a65558c366df0054b46d5453
Veera Sankar <sankarsivam6991@gmail.com>
2026-01-12 21:23:46 +0530

    created signin and sign up screens

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endPoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: "com.gtx.fooddelivery",
+  databaseId:'6964ce1f00078dcbb0f8',
+  userCollectionId:"user"
+};
